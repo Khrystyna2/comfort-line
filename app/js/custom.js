dce06b4611b9df0f0f7be679e88f19e1b3cabf53
Khrystyna <l.xrustja@gmail.com>
2019-07-18 08:24:24 +0300

    first commit

---
+++ app/js/custom.js
@@ -0,0 +1,34 @@
+$(window).on("load", function() {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		$("body").addClass("ios");
+	} else {
+		$("body").addClass("web");
+	}
+});
+
+$(document).ready(function() {
+	// if($('.slick-slider').length) {
+	// 	$('.slick-slider').slick({
+	// 		dots: true,
+	// 		infinite: false,
+	// 		speed: 300,
+	// 		slidesToShow: 4,
+	// 		slidesToScroll: 4,
+	// 		responsive: [
+	// 			{
+	// 			  breakpoint: 1024,
+	// 			  settings: {
+	// 				slidesToShow: 3,
+	// 				slidesToScroll: 3,
+	// 				infinite: true,
+	// 				dots: true
+	// 			  }
+	// 			},
+	// 			{
+	// 			  breakpoint: 600,
+	// 			  settings: "unslick"
+	// 			}
+	// 		]
+	// 	});
+	// };
+});
