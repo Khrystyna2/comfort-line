cfe0cf1fbe0a4c2cbd30e937a3005848b003eed7
Khrystyna <l.xrustja@gmail.com>
2019-07-22 08:00:58 +0300

    added 2 section in page

--- app/js/custom.js
+++ app/js/custom.js
@@ -7,77 +7,87 @@ $(window).on("load", function() {
 });
 
 $(document).ready(function() {
-
-	$('.phone .arrow-down').click(function() {
-		$(this).next().slideToggle()
-	})
+	$(".phone .arrow-down").click(function() {
+		$(this)
+			.next()
+			.slideToggle();
+	});
 
 	// slider first screen
-    $(function () {
-        $('.slider').on('init', function (event, slick) {
-            let findBlock = $(this).find('.total');
-            let findCurrentBlock = $(this).find('.current');
-            findCurrentBlock.text('0' + (slick.currentSlide + 1));
-            if (slick.slideCount < 10) {
-                findBlock.text('0' + slick.slideCount);
-            } else {
-                findBlock.text(slick.slideCount);
-            }
-        })
-            .slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                fade: true,
-                // autoplay: true,
-                autoplaySpeed: 2000,
-                focusOnSelect: false,
+	$(function() {
+		$(".slider")
+			.on("init", function(event, slick) {
+				let findBlock = $(this).find(".total");
+				let findCurrentBlock = $(this).find(".current");
+				findCurrentBlock.text("0" + (slick.currentSlide + 1));
+				if (slick.slideCount < 10) {
+					findBlock.text("0" + slick.slideCount);
+				} else {
+					findBlock.text(slick.slideCount);
+				}
+			})
+			.slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				fade: true,
+				// autoplay: true,
+				autoplaySpeed: 2000,
+				focusOnSelect: false,
 				slide: ".slider__item",
 				nextArrow: ".slider__next",
 				prevArrow: ".slider__prev",
-                speed: 1000,
-                responsive: [
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            dots: false
-                        }
-                    }
-                ]
-            })
-            .on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-                let indexNextSlider = nextSlide + 1;
-                let findCurrentBlock = $(this).find('.current');
-                if (indexNextSlider <= 9) {
-                    findCurrentBlock.text('0' + (nextSlide + 1));
-                }
-                else {
-                    findCurrentBlock.text(nextSlide + 1);
-                }
-            });
-    });
+				speed: 1000,
+				responsive: [
+					{
+						breakpoint: 992,
+						settings: {
+							dots: false
+						}
+					}
+				]
+			})
+			.on("beforeChange", function(event, slick, currentSlide, nextSlide) {
+				let indexNextSlider = nextSlide + 1;
+				let findCurrentBlock = $(this).find(".current");
+				if (indexNextSlider <= 9) {
+					findCurrentBlock.text("0" + (nextSlide + 1));
+				} else {
+					findCurrentBlock.text(nextSlide + 1);
+				}
+			});
+	});
 
-	// if($('.slick-slider').length) {
-	// 	$('.slick-slider').slick({
-	// 		dots: true,
-	// 		infinite: false,
-	// 		speed: 300,
-	// 		slidesToShow: 4,
-	// 		slidesToScroll: 4,
-	// 		responsive: [
-	// 			{
-	// 			  breakpoint: 1024,
-	// 			  settings: {
-	// 				slidesToShow: 3,
-	// 				slidesToScroll: 3,
-	// 				infinite: true,
-	// 				dots: true
-	// 			  }
-	// 			},
-	// 			{
-	// 			  breakpoint: 600,
-	// 			  settings: "unslick"
-	// 			}
-	// 		]
-	// 	});
-	// };
+	$(
+		'<div class="spinner__nav"><div class="spinner__btn spinner-up">+</div><div class="spinner__btn spinner-down">-</div></div>'
+	).insertAfter(".spinner>input");
+	$(".spinner").each(function() {
+		let spinner = $(this),
+			input = spinner.find('input[type="number"]'),
+			btnUp = spinner.find(".spinner-up"),
+			btnDown = spinner.find(".spinner-down"),
+			min = input.attr("min"),
+			max = input.attr("max");
+
+		btnUp.click(function() {
+			let oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				newVal = oldValue;
+			} else {
+				newVal = oldValue + 100;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+		btnDown.click(function() {
+			let oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				newVal = oldValue;
+			} else {
+				newVal = oldValue - 100;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+	});
 });
